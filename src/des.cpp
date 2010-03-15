864a85671e6b76256ebe432b59c02dc15876bea9
Brian Gianforcaro <b.gianfo@gmail.com>
2010-03-14 23:46:22 -0400

    Addition of some more matricies as well as permutation funcitons.

--- src/des.cpp
+++ src/des.cpp
@@ -10,7 +10,73 @@
 
 #include "des.h"
 
+/* Shift's change depending on which round we are on */
+static unsigned char SHIFTS[ROUNDS] = {1,1,2,2,2,2,2,2,1,2,2,2,2,2,2,1};
 
+/* Primative functino P pg: 22 of DES spec */
+static unsigned char P[32] = {
+  16,  7, 20, 21,
+  29, 12, 28, 17,
+   1, 15, 23, 26,
+   5, 18, 31, 10,
+   2,  8, 24, 14,
+  32, 27,  3,  9,
+  19, 13, 30,  6,
+  22, 11,  4, 25 
+};
+
+static unsigned char E[48] = {
+  32,  1,  2,  3,  4,  5, 
+   4,  5,  6,  7,  8,  9, 
+   8,  9, 10, 11, 12, 13, 
+  12, 13, 14, 15, 16, 17, 
+  16, 17, 18, 19, 20, 21, 
+  20, 21, 22, 23, 24, 25, 
+  24, 25, 26, 27, 28, 29, 
+  28, 29, 30, 31, 32,  1
+};
+
+/* Permiated choice #1 pg: 23 of DES spec */
+static unsigned char PC1[56] = {
+  57, 49, 41, 33, 25, 17,  9,
+   1, 58, 50, 42, 34, 26, 18,
+  10,  2, 59, 51, 43, 35, 27,
+  19, 11,  3, 60, 52, 44, 36,
+  63, 55, 47, 39, 31, 23, 15,
+   7, 62, 54, 46, 38, 30, 22,
+  14,  6, 61, 53, 45, 37, 29,
+  21, 13,  5, 28, 20, 12,  4
+};
+
+unsigned char PC2[56] = {
+
+};
+
+/* IP prime pg: 14 */
+static unsigned char IP[64] = {
+  58, 50, 42, 34, 26, 18, 10, 2, 
+  60, 52, 44, 36, 28, 20, 12, 4, 
+  62, 54, 46, 38, 30, 22, 14, 6, 
+  64, 56, 48, 40, 32, 24, 16, 8, 
+  57, 49, 41, 33, 25, 17,  9, 1, 
+  59, 51, 43, 35, 27, 19, 11, 3, 
+  61, 53, 45, 37, 29, 21, 13, 5, 
+  63, 55, 47, 39, 31, 23, 15, 7 
+};
+
+/* IP prime pg: 14 */
+static unsigned char IPP[64] = {
+  40,  8, 48, 16, 56, 24, 64, 32, 
+  39,  7, 47, 15, 55, 23, 63, 31, 
+  38,  6, 46, 14, 54, 22, 62, 30, 
+  37,  5, 45, 13, 53, 21, 61, 29, 
+  36,  4, 44, 12, 52, 20, 60, 28, 
+  35,  3, 43, 11, 51, 19, 59, 27, 
+  34,  2, 42, 10, 50, 18, 58, 26, 
+  33,  1, 41,  9, 49, 17, 57, 25
+};
+
+/* All S-Boxes  */
 unsigned char SP[8][4][16] = {
 
   /* S1 function pg: 19 of DES spec */
@@ -79,3 +145,43 @@ unsigned char SP[8][4][16] = {
 
 };
 
+
+des::des( char* block , char* key ) {
+  this->block = block;
+  this->key = key;
+}
+
+des::~des( ) {
+  delete this->block;
+  delete this->key;
+}
+
+/*
+ * Implements the main enciphering algorithm in figure 1 pg: 13 DES spec
+ */
+void des::encrypt() {
+  this->permiate();
+
+  this->inv_permiate();
+  return;
+}
+
+/*
+ * Scramble the plain text a little bit
+ */
+void des::permiate( void ) {
+  for( int i = 0; i < 64; i++ ) {
+    this->block[i] = this->block[ IP[i] - 1 ];
+  }
+  return;
+}
+
+/*
+ * Inverse scramble the plain text a little bit
+ */
+void des::inv_permiate( void ) {
+  for( int i = 0; i < 64; i++ ) {
+    this->block[i] = this->block[ IPP[i] - 1 ];
+  }
+  return;
+}
--- src/des.h
+++ src/des.h
@@ -10,4 +10,28 @@
 #define _DES_H_
 
 
+#define ROUNDS 16
+
+class des {
+
+  private:
+
+    char* block;
+    char* key;
+
+    void inv_permiate( void );
+
+    void permiate( void );
+
+  public:
+
+    des( char* block , char* key );
+
+    ~des();
+
+    void encrypt();
+
+    void decrypt( void );
+};
+
 #endif
